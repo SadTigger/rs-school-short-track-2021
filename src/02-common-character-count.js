08fbb68719994544b8459d2391a3621610d705a7
Pavel Zhachko <rammiel@yandex.ru>
2021-04-12 20:53:33 +0300

    feat: add getCommonCharacterCount solution after delete prev fork

--- src/02-common-character-count.js
+++ src/02-common-character-count.js
@@ -9,8 +9,24 @@
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new Error('Not implemented');
+function getCommonCharacterCount(s1, s2) {
+  const arr1 = s1.split('').sort();
+  const arr2 = s2.split('').sort();
+  const unique1 = [...new Set(arr1)];
+  const unique2 = [...new Set(arr2)];
+  const union = [...new Set([...arr1, ...arr2])];
+  const obj1 = {};
+  const obj2 = {};
+  unique1.forEach((value) => { obj1[value] = arr1.filter((str) => (str === value)).length; });
+  unique2.forEach((value) => { obj2[value] = arr2.filter((str) => (str === value)).length; });
+  let res = 0;
+  union.forEach((el) => {
+    const differ = Math.max(obj1[el], obj2[el]) - Math.min(obj1[el], obj2[el]);
+    if (!Number.isNaN(differ)) {
+      res += Math.max(obj1[el], obj2[el]) - differ;
+    }
+  });
+  return res;
 }
 
 module.exports = getCommonCharacterCount;
