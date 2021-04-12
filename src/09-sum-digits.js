06d3f95923a54b5c190db91e8cbbb9a4ea9ad6d1
Pavel Zhachko <rammiel@yandex.ru>
2021-04-12 21:00:56 +0300

    feat: add getSumOfDigits solution after delete prev fork

--- src/09-sum-digits.js
+++ src/09-sum-digits.js
@@ -10,8 +10,11 @@
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new Error('Not implemented');
+function getSumOfDigits(n) {
+  const stringN = String(n);
+  if (stringN.length === 1) return n;
+  const array = stringN.split('').reduce((a, b) => Number(a) + Number(b));
+  return getSumOfDigits(array);
 }
 
 module.exports = getSumOfDigits;
