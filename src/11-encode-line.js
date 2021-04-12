31810cee3146ac725eea384656dbfc4019e8e5bf
Pavel Zhachko <rammiel@yandex.ru>
2021-04-12 21:03:23 +0300

    feat: add encodeLine solution after delete prev fork

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,21 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const arr = str.split('');
+  const result = [];
+  arr.forEach((el) => {
+    const len = arr.filter((string) => string === el).length;
+    const regexp = new RegExp(el.repeat(len));
+    if (len === 1) {
+      result.push(el);
+    } else if (!regexp.test(str)) {
+      result.push(el);
+    } else if (result.indexOf(`${len}${el}`) === -1) {
+      result.push(`${len}${el}`);
+    }
+  });
+  return result.flat(Infinity).join('');
 }
 
 module.exports = encodeLine;
